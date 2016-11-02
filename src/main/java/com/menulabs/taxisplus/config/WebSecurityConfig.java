9d514fef33b1c01d8d4ca5e802621977523e03c7
Miguel Gonzalez <maggonzz@gmail.com>
2016-11-03 00:33:36 +0100

    updated security config

--- src/main/java/com/menulabs/taxisplus/config/WebSecurityConfig.java
+++ src/main/java/com/menulabs/taxisplus/config/WebSecurityConfig.java
@@ -24,7 +24,7 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/", "/public/**").permitAll()
-                .antMatchers("/usuarios/**").hasAuthority("ADMINCAPTURER")
+                .antMatchers("/usuarios/**", "/socios/**").hasAuthority("ADMINCAPTURER")
                 .anyRequest().fullyAuthenticated()
                 .and()
                 .formLogin()
